d0e3d5aeb332bdcaa71872876f9c2eda73e24aa4
ssasakiseiji <ssasakiseiji@gmail.com>
2026-01-26 15:39:26 -0300

    exercise 2.11.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import NameList from './components/NameList'
 import PersonForm from './components/PersonForm'
 import Search from './components/Search'
+import axios from 'axios' 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',  number:'0999699699', id: 1},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState("")
@@ -50,6 +46,14 @@ const App = () => {
       setNewName("")
       setNewNumber("")
   }
+
+  const initialFetch = () => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => setPersons(response.data))
+  }
+
+  useEffect(initialFetch, [])
 
   const personsToShow = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
 
