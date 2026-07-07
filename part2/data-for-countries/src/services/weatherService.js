1f56148487291aff46ee871e2ee59cf18798740a
ssasakiseiji <ssasakiseiji@gmail.com>
2026-07-07 12:42:28 -0300

    leak fix - revoked api key

--- part2/data-for-countries/src/services/weatherService.js
+++ part2/data-for-countries/src/services/weatherService.js
@@ -5,7 +5,7 @@ const weather_api_key = import.meta.env.VITE_WEATHER_API_KEY
 const getWeather = (country) => {
     return (
         axios
-        //example: .get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=445b10d12f9f775406440817d6847646")
+        //example: .get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=...api key")
             .get(`https://api.openweathermap.org/data/2.5/weather?q=${country}&APPID=${weather_api_key}`)
             .then(response=> response.data
              )
